c94d115a24363ad69ef385536c8c32681d542ff0
Steve Halasz <skh2@cornell.edu>
2018-08-21 12:57:51 -0400

    Show fizzbuzzness on application's main index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'fizzbuzz#index'
   get 'fizzbuzz/index'
   get 'fizzbuzz/:id', to: 'fizzbuzz#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
